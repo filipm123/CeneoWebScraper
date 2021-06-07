e9749de608407fe4acdbbfc6e6672186f4ae2edf
filipor123 <filipor123@gmail.com>
2021-06-07 16:34:46 +0200

    2021-06-07 FM

--- app/models/product.py
+++ app/models/product.py
@@ -3,23 +3,20 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-
 class Product:
-    def __init__(self, product_id, product_name=None, opinions=[]):
+    def __init__(self, product_id, product_name = None, opinions = []):
         self.product_id = product_id
         self.product_name = product_name
-        self.opinions = opinions
-
+        self.opinions = opinions.copy()
+    
     def extract_product(self):
-        next_page = "https://www.ceneo.pl/{}#tab=reviews".format(
-            self.product_id)
+        next_page = "https://www.ceneo.pl/{}#tab=reviews".format(self.product_id)
         while next_page:
             respons = requests.get(next_page)
             page_dom = BeautifulSoup(respons.text, "html.parser")
             opinions = page_dom.select("div.js_product-review")
             for opinion in opinions:
-                self.opinions.append(Opinion().extract_opinion(
-                    opinion).transform_opinion())
+                self.opinions.append(Opinion().extract_opinion(opinion).transform_opinion())
             try:
                 next_page = "https://www.ceneo.pl" + \
                     page_dom.select("a.pagination__next").pop()["href"]
@@ -43,9 +40,13 @@ class Product:
     def save_to_json(self):
         with open(f"app/products/{self.product_id}.json", "w", encoding="UTF-8") as fp:
             json.dump(self.to_dict(), fp, indent=4, ensure_ascii=False)
+        fp.close()
+
     def read_from_json(self):
+        print(self.product_id)
         with open(f"app/products/{self.product_id}.json", "r", encoding="UTF-8") as fp:
             prod = json.load(fp)
+        fp.close()
         self.product_name = prod['product_name']
         opinions = prod['opinions']
         for opinion in opinions:
